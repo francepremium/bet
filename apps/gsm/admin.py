b6f24d8f4d4799cfdc3fb0d5159444a0872b4573
James Pic <jamespic@gmail.com>
2011-04-26 09:26:31 +0200

    Improved admin interface

--- apps/gsm/admin.py
+++ apps/gsm/admin.py
@@ -6,21 +6,27 @@ from models import *
 admin.site.register(Sport)
 
 class TourAdmin(TranslationAdmin):
-    pass
+    list_display = ('gsm_id', 'sport', 'name')
+    list_filter = ('sport', 'last_updated')
+    search_fields = ('name_fr', 'name_en')
 
 admin.site.register(Tour, TourAdmin)
 
 class SeasonAdmin(TranslationAdmin):
-    pass
+    list_display = ('gsm_id', 'competition', 'name')
+    list_filter = ('last_updated', 'competition__sport',)
+    search_fields = ('competition__name_fr', 'competition__name_en', 'name_fr', 'name_en')
 
 admin.site.register(Season, SeasonAdmin)
 
 class CompetitionAdmin(TranslationAdmin):
-    pass
+    list_display = ('gsm_id', 'sport', 'area', 'tour', 'name')
+    list_filter = ('sport',)
+    search_fields = ('area__name_fr', 'area__name_en', 'tour__name_fr', 'tour__name_en', 'name_fr', 'name_en')
 
 admin.site.register(Competition, CompetitionAdmin)
 
 class AreaAdmin(TranslationAdmin):
-    list_display = ('gsm_id', 'country_code', 'name')
+    list_display = ('gsm_id', 'country_code', 'parent', 'name')
 
 admin.site.register(Area, AreaAdmin)
