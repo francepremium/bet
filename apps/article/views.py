d0c7f19127e6e8f3049472e2eeb4aa70071e4417
James Pic <jamespic@gmail.com>
2011-06-07 14:12:36 +0200

    Removed dead code

--- apps/article/views.py
+++ apps/article/views.py
@@ -36,22 +36,3 @@ class ArticleUpdateView(generic.UpdateView):
         if obj.creation_user != self.request.user and not request.user.is_staff:
             return http.HttpResponseForbidden(_('Not your article, and you are not staff'))
         return obj
-
-@login_required
-def article_form(request, object_id=None, form_class=None,
-    template_name='article/article_form.html', extra_context=None):
-    if object_id:
-        instance = Article.objects.get(pk=object_id)
-    else:
-        instance = Article(user=request.user)
-
-    if request.method == 'POST':
-        form = form_class(request.POST, instance=instance)
-        if form.is_valid():
-            article = form.save()
-            messages.success(request, _('article saved'))
-            return shortcuts.redirect(article.get_absolute_url())
-    else:
-        form = form_class(instance=instance)
-    
-    context['form'] = form
