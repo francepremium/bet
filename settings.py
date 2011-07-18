5835dd26c0380a3172ea23547b2120277e08f1a0
James Pic <jamespic@gmail.com>
2011-07-18 11:00:15 +0200

    Moved non server specific settings from local_settings.py to settings.py for gondor support

--- settings.py
+++ settings.py
@@ -154,6 +154,118 @@ DEBUG_TOOLBAR_CONFIG = {
     "INTERCEPT_REDIRECTS": False,
 }
 
+
+
+MIDDLEWARE_CLASSES = [
+    'localeurl.middleware.LocaleURLMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'pagination.middleware.PaginationMiddleware'
+]
+
+
+TEMPLATE_CONTEXT_PROCESSORS = [
+    'django.core.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.request',
+    'django.contrib.messages.context_processors.messages',
+    'staticfiles.context_processors.static_url',
+    'pinax.core.context_processors.pinax_settings',
+    'bet.context_processors.incomplete_ticket',
+]
+
+INSTALLED_APPS = [
+    'localeurl',
+    # Django
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.humanize',
+    'django.contrib.comments',
+    
+    'pinax.templatetags',
+    'emailconfirmation',
+    'pinax.apps.account',
+    
+    # external
+    'modeltranslation',
+    'staticfiles',
+    'debug_toolbar',
+    'mailer',
+    'uni_form',
+    'django_openid',
+    'ajax_validation',
+    'timezones',
+    'emailconfirmation',
+    'django_extensions',
+    'django_filters',
+    'pagination',
+    'ajax_select',
+    'sentry.client',
+    'avatar',
+    'actstream',
+    'endless_pagination',
+    'autofixture',
+    # 'postman',
+    'south',
+    
+    # Pinax
+    
+    # project
+    'gsm',
+    'bookmaker',
+    'bet',
+    'clan',
+    'scoobet',
+    'article',
+]
+
+LANGUAGE_CODE = 'fr_FR'
+TIME_ZONE = 'Europe/Paris'
+LANGUAGES = (
+    ('en', gettext('English')),
+    ('fr', gettext('French')),
+)
+MODELTRANSLATION_DEFAULT_LANGUAGE='fr'
+MODELTRANSLATION_TRANSLATION_REGISTRY='translation'
+
+GSM_USERNAME = 'cportal'
+GSM_PASSWORD = 'client'
+GSM_LANGUAGE = 'fr'
+GSM_URL = 'http://%s:%s@webpull.globalsportsmedia.com' % (
+    GSM_USERNAME,
+    GSM_PASSWORD,
+)
+GSM_CACHE = os.path.join(PROJECT_ROOT, 'cache', 'gsm')
+
+ACCOUNT_OPEN_SIGNUP = True
+LOGIN_URL='/account/login/'
+
+AJAX_LOOKUP_CHANNELS = {
+    'session': ('gsm.lookups', 'SessionLookup'),
+    'user': {'model': 'auth.User', 'search_field':'username'},
+}
+
+ACCOUNT_EMAIL_VERIFICATION = False
+EMAIL_CONFIRMATION_DAYS = 3
+
+POSTMAN_DISALLOW_ANONYMOUS=True
+POSTMAN_DISALLOW_MULTIRECIPIENTS=True
+POSTMAN_DISALLOW_COPIES_ON_REPLY=False
+POSTMAN_AUTO_MODERATE_AS=True
+POSTMAN_AUTOCOMPLETER_APP={
+    'arg_default': 'user',
+}
+
+
 # local_settings.py can be used to override environment-specific settings
 # like database and email that differ between development and production.
 try:
