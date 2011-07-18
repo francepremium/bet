a1c6e3831642be98b42a17e3c5e12718f358c492
James Pic <jamespic@gmail.com>
2011-07-18 10:40:16 +0200

    Fix absolute path in wsgi.py

--- deploy/wsgi.py
+++ deploy/wsgi.py
@@ -9,7 +9,7 @@ from site import addsitedir
 sys.path.insert(0, abspath(join(dirname(__file__), "../../")))
 
 from django.conf import settings
-os.environ["DJANGO_SETTINGS_MODULE"] = "main.settings"
+os.environ["DJANGO_SETTINGS_MODULE"] = "settings"
 
 sys.path.insert(0, join(settings.PROJECT_ROOT, "apps"))
 
