f429464f0302f04aa0b12a54b36f9fac637dc315
James Pic <jamespic@gmail.com>
2011-07-19 09:15:45 +0200

    Added a drop in replacement for runserver, run pip install -r requirements/project.txt

--- settings.py
+++ settings.py
@@ -217,6 +217,7 @@ INSTALLED_APPS = [
     'autofixture',
     # 'postman',
     'south',
+    'devserver',
     
     # Pinax
     
@@ -266,6 +267,17 @@ POSTMAN_AUTOCOMPLETER_APP={
     'arg_default': 'user',
 }
 
+DEVSERVER_MODULES = (
+    'devserver.modules.sql.SQLRealTimeModule',
+    'devserver.modules.sql.SQLSummaryModule',
+    'devserver.modules.profile.ProfileSummaryModule',
+
+    # Modules not enabled by default
+    'devserver.modules.ajax.AjaxDumpModule',
+    'devserver.modules.profile.MemoryUseModule',
+    'devserver.modules.cache.CacheSummaryModule',
+    'devserver.modules.profile.LineProfilerModule',
+)
 
 # local_settings.py can be used to override environment-specific settings
 # like database and email that differ between development and production.
