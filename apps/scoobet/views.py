9303c443997e98ce4e17ff6458d08b6f7a39b0ce
James Pic <jamespic@gmail.com>
2011-07-20 19:56:09 +0200

    Fixed a bug with the up arrow key and cleaned autocomplete stuff

--- apps/scoobet/views.py
+++ apps/scoobet/views.py
@@ -15,6 +15,25 @@ from actstream.models import actor_stream, Follow
 
 from bet.helpers import *
 from bet.models import *
+from gsm.models import Session, Competition, GsmEntity
+
+def autocomplete(request,
+    template_name='scoobet/autocomplete.html', extra_context=None):
+    q = request.GET['q'] # crash if q is not in the url
+
+    context = {}
+    context['sessions'] = Session.objects.filter(
+        Q(oponnent_A_name=q)|Q(oponnent_B_name__istartswith=q)).distinct()[:3]
+    context['users'] = User.objects.filter(username__icontains=q)[:3]
+    context['teams'] = GsmEntity.objects.filter(
+        name__icontains=q, tag='team')[:3]
+    context['players'] = GsmEntity.objects.filter(
+        name__icontains=q, tag='person')[:3]
+    context['competitions'] = Competition.objects.filter(
+        name__icontains=q)[:3]
+
+    return shortcuts.render_to_response(template_name, context,
+        context_instance=template.RequestContext(request))
 
 @login_required
 def me(request):
--- apps/yourlabs/autocomplete/views.py
+++
@@ -1,7 +0,0 @@
-from django.views.generic.list import ListView
-
-from bet.models import *
-
-class AutocompleteView(ListView):
-    template_name_suffix = '_autocomplete'
-    queryset = Bet.objects.all()
