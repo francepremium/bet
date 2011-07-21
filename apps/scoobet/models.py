ef67d99838d463c5188d48c15f868b62849b16f3
James Pic <jamespic@gmail.com>
2011-07-21 15:46:47 +0200

    Removed select_related from User friends/follows/followers methods to prevent from spawning unnecessary joins

--- apps/scoobet/models.py
+++ apps/scoobet/models.py
@@ -78,7 +78,7 @@ def user_friends(user):
     target_choices_qs = User.objects.filter(
         Q(follow__object_id=user.pk, follow__content_type=c) | 
         Q(id__in=follows_users_ids)
-    ).select_related()
+    )
     return target_choices_qs
 User.friends = user_friends
 
@@ -90,7 +90,7 @@ def user_follows(user):
                                               content_type__model='user') \
                                       .exclude(object_id=user.pk) \
                                       .values_list('object_id', flat=True)
-    return User.objects.filter(pk__in=follows_users_ids).select_related()
+    return User.objects.filter(pk__in=follows_users_ids)
 User.follows = user_follows
 
 def user_following(user):
@@ -98,5 +98,5 @@ def user_following(user):
 
     followers_qs = User.objects.filter(follow__object_id=user.pk, 
                                         follow__content_type=c)
-    return followers_qs.select_related()
+    return followers_qs
 User.following = user_following
