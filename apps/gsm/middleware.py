adba234db439ce8e038193533fd702c579079480
James Pic <jamespic@gmail.com>
2011-07-28 12:19:44 +0200

    Timezone support for anonymous user

--- apps/gsm/middleware.py
+++ apps/gsm/middleware.py
@@ -1,14 +1,17 @@
 import pytz, datetime
 
+from django.conf import settings
 from timezones.utils import adjust_datetime_to_timezone
 
 class TimezoneMiddleware(object):
     def process_request(self, request):
         request.timezone = request.session.get('timezone', {})
-        print request.timezone, request.session.get('timezone', False)
         
-        if not request.timezone.keys() and request.user.is_authenticated():
-            request.timezone['string'] = request.user.account_set.all()[0].timezone
+        if not request.timezone.keys():
+            if request.user.is_authenticated():
+                request.timezone['string'] = request.user.account_set.all()[0].timezone
+            else:
+                request.timezone['string'] = settings.TIME_ZONE
 
             ref = datetime.datetime.now()
             timezone = pytz.timezone(request.timezone['string'])            
