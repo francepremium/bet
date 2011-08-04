88165d75c938f502d000a220386ee78ae2bccb30
James Pic <jamespic@gmail.com>
2011-08-04 09:04:40 +0200

    Got rid of celery in favor of uwsgi

--- local_settings.py
+++ local_settings.py
@@ -8,22 +8,24 @@ gettext = lambda s: s
 DATABASES = {
     'default': {
         'ENGINE': 'postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'bet.yourlabs.org',                       # Or path to database file if using sqlite3.
-        'USER': 'bet.yourlabs.org',                             # Not used with sqlite3.
-        'PASSWORD': 'stnuSNE8OUTNhstnoeuasnt',                         # Not used with sqlite3.
+        'NAME': 'bet_prod',                       # Or path to database file if using sqlite3.
+        'USER': 'bet_prod',                             # Not used with sqlite3.
+        'PASSWORD': 'stnuSNE8OUToeuTEUSHNNTtsnhsntoheu987987EUHTD',                         # Not used with sqlite3.
         'HOST': '',                             # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                             # Set to empty string for default. Not used with sqlite3.
     }
 }
 
 
-DEBUG=True
-
 SENTRY_KEY = 'ENUTntheou)(098eu)(E0U983$@#$@34342oasuth90$#@$#@'
 SENTRY_REMOTE_URL = 'http://beta.yourlabs.org/sentry/store/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = "!0!i2uocz=ks1yxvlx*x5o6sn$n_h6a5r5n8@k+qn$d4q+@pnq"
+
+DEBUG=True
+TEMPLATE_DEBUG=DEBUG
+SERVE_MEDIA=DEBUG
 
 try:
     from personal_settings import *
