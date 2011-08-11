bd22dfc4fce89905fc94cbbbd710bd0ea36e9f17
James Pic <jamespic@gmail.com>
2011-08-11 09:49:55 +0200

    Fixed support for localeurl in beta middleware

--- apps/beta/middleware.py
+++ apps/beta/middleware.py
@@ -3,16 +3,14 @@ from django.conf import settings
 from django.core import urlresolvers
 
 try:
-    BETA_URL = urlresolvers.reverse('beta_homepage')
-except:
-    raise Exception('Please include beta.urls in your urls')
+    from localeurl.templatetags.localeurl_tags import rmlocale
+except ImportError:
+    rmlocale = lambda x: x
 
 try:
     BYPASS_URLS = settings.BETA_BYPASS_URLS
 except:
     BYPASS_URLS = []
-
-BYPASS_URLS.append(BETA_URL)
 
 for test in ['acct_passwd_reset', 'acct_passwd_reset_done', 'acct_passwd_reset_key']:
     try:
@@ -29,12 +27,14 @@ for test in ['STATIC_URL', 'MEDIA_URL', 'LOGIN_URL']:
 if 'localeurl.middleware.LocaleURLMiddleware' in settings.MIDDLEWARE_CLASSES:
     for code, label in settings.LANGUAGES:
         for i in range(0, len(BYPASS_URLS)):
-            if BYPASS_URLS[i][0:3] == '/' + code:
-                BYPASS_URLS[i] = BYPASS_URLS[i][3:]
+            BYPASS_URLS[i] = rmlocale(BYPASS_URLS[i])
 
 class LoginMiddleware(object):
     def process_request(self, request):
         path = request.path_info
+        BETA_URL = rmlocale(urlresolvers.reverse('beta_homepage'))
+        if not BETA_URL in BYPASS_URLS:
+            BYPASS_URLS.append(BETA_URL)
 
         for url in BYPASS_URLS:
             if path.find(url) == 0:
