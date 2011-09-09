33d87b2060a7d3c42791d08a49a39ccec96ea152
James Pic <jamespic@gmail.com>
2011-09-09 17:43:27 +0200

    Added a little cooldown time in tasks

--- tasks.py
+++ tasks.py
@@ -1,18 +1,26 @@
+import time.sleep()
+from yourlabs import runner
+
 from django.core.management import call_command
 
 from gsm.management.commands.gsm_sync import Command as GsmSyncCommand
 
-def send_mail(args={}):
+def send_mail():
     call_command('send_mail')
+    time.sleep(3*60)
 
 def retry_deferred():
     call_command('retry_deferred')
+    time.sleep(3*60)
 
 def gsm_sync():
     GsmSyncCommand().handle(cooldown=3)
+    time.sleep(3*60)
 
-def gsm_sync_live(args={}):
+def gsm_sync_live():
     call_command('gsm_sync_live')
+    time.sleep(5)
 
 def update_index():
     call_command('update_index')
+    time.sleep(3*60)
