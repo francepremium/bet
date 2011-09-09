a904f4e6132c6c4bee39dbcb35cc79d8e60b6df6
James Pic <jamespic@gmail.com>
2011-09-09 11:18:56 +0200

    Second attempt, fails

--- tasks.py
+++ tasks.py
@@ -1,3 +1,4 @@
+import logging
 import time
 import uwsgi
 
@@ -6,16 +7,36 @@ from tasksconsumer import *
 
 from gsm.management.commands.gsm_sync import Command as GsmSyncCommand
 
-@queueconsumer('send_mail')
-def send_mail(args={}):
-    call_command('send_mail')
-    call_command('retry_deferred')
-    time.sleep(30)
-    enqueue(queue='send_mail')
+logger = logging.getLogger('gsm')
+
+logger.info('imported')
 
 @queueconsumer('gsm_sync')
 def gsm_sync(args={}):
-    GsmSyncCommand().handle(cooldown=3)
-    call_command('update_index')
-    time.sleep(30)
+    logger.info('started gsm_sync function')
+    return uwsgi.SPOOL_RETRY
+    cooldowns = (1,2,3,4)
+    attempt = 0
+    while attempt <= len(cooldowns):
+        e = None
+        try:
+            logger.info('started gsm_sync actual task')
+            raise Exception()
+        except Exception as e:
+            logger.info("Caught %s while doing gsm_sync for the %s th time, sleep %s seconds and retry" % (e, attempt, cooldowns[attempt]))
+            time.sleep(cooldowns[attempt])
+            attempt += 1
+
+    if e is not None:
+        logger.error('Failed %s times' % len(cooldowns))
+        # notify admins...
+
     enqueue(queue='gsm_sync')
+    logger.info('enqueued self again')
+
+
+    # old code
+    #GsmSyncCommand().handle(cooldown=3)
+    #call_command('update_index')
+    #time.sleep(30)
+
