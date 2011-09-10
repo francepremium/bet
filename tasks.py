99e145c02f1140ba3017430ca16528d0ca35c88d
James Pic <jamespic@gmail.com>
2011-09-10 08:01:05 +0200

    Adjust to server hardware

--- tasks.py
+++ tasks.py
@@ -7,20 +7,20 @@ from gsm.management.commands.gsm_sync import Command as GsmSyncCommand
 
 def send_mail():
     call_command('send_mail')
-    time.sleep(3*60)
+    time.sleep(5*60)
 
 def retry_deferred():
     call_command('retry_deferred')
-    time.sleep(3*60)
+    time.sleep(5*60)
 
 def gsm_sync():
     GsmSyncCommand().handle(cooldown=3)
-    time.sleep(3*60)
+    time.sleep(20*60)
 
 def gsm_sync_live():
     call_command('gsm_sync_live')
-    time.sleep(3)
+    time.sleep(7)
 
 def update_index():
     call_command('update_index')
-    time.sleep(3*60)
+    time.sleep(5*60)
