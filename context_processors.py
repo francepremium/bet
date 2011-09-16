6aa7709eae225f0e1e32567bdaafb3e456bb8c80
James Pic <jamespic@gmail.com>
2011-09-16 08:28:09 +0200

    Fugbix in language code saver

--- context_processors.py
+++ context_processors.py
@@ -3,18 +3,18 @@ from django.conf import settings
 def save_user_locale(request):
     if not request.user.is_authenticated():
         return {}
-   
-    if 'LANGUAGE_CODE' not in request.session:
+
+    if 'LANGUAGE_CODE' not in request.session.keys():
         account = request.user.account_set.all()[0]
-        request.session['LANGUAGE_CODE'] = account.language.split('_')[0]
+        request.session['LANGUAGE_CODE'] = account.language
 
     if request.session['LANGUAGE_CODE'] != request.LANGUAGE_CODE:
         account = request.user.account_set.all()[0]
         for value, label in settings.LANGUAGES:
-            if value.split('_')[0] == request.LANGUAGE_CODE:
+            if value == request.LANGUAGE_CODE:
                 break
         account.language = value
+        request.session['LANGUAGE_CODE'] = account.language
         account.save()
-        request.session['LANGUAGE_CODE'] = request.LANGUAGE_CODE
-    
+
     return {}
