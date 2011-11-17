35f02bc6524773fe09aec4673a27b69790d87b72
jpic <jpic@yourlabs.org>
2011-11-17 10:27:51 +0100

    BetAdmin configuration

--- apps/bet/admin.py
+++ apps/bet/admin.py
@@ -14,6 +14,22 @@ class TicketAdmin(admin.ModelAdmin):
 
 class BetAdmin(admin.ModelAdmin):
     raw_id_fields = ('session',)
+    list_display = (
+        'bettype',
+        'choice',
+        'session',
+        'odds',
+        'flagged',
+        'correction',
+    )
+    list_filter = (
+        'flagged',
+    )
+    list_editable = (
+        'flagged',
+        'correction',
+        'odds',
+    )
     def render_change_form(self, request, context, add=False, change=False, form_url='', obj=None):
         context['myvalue'] = context['adminform'].form.instance
         return super(BetAdmin, self).render_change_form(request, context, add, change, form_url, obj)
