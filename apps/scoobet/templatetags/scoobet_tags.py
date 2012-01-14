a5d354f352587a71b838a938829fcef13b234384
jpic <jpic@yourlabs.org>
2012-01-14 15:58:49 +0100

    Fix in url regexp of render_comment

--- apps/scoobet/templatetags/scoobet_tags.py
+++ apps/scoobet/templatetags/scoobet_tags.py
@@ -12,11 +12,11 @@ from gsm.models import AbstractGsmEntity, Sport
 
 register = template.Library()
 
-URL_REGEXP = r'(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)((?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:".,?])))'
+URL_REGEXP = r'((?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)((?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:".,?]))))'
 
 @register.filter
 def render_comment(text):
-    text = re.sub(URL_REGEXP, r'<a href="\1">\2</a>', text)
+    text = re.sub(URL_REGEXP, r'<a href="\1">\3</a>', text)
     text = linebreaksbr(text.strip())
     return text
 
