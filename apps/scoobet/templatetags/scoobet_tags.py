15bbf48a860242fb86950a2ccef57ef100f8ab1f
jpic <jpic@yourlabs.org>
2012-01-14 15:41:20 +0100

    Comments support urls and linebreaks

--- apps/scoobet/templatetags/scoobet_tags.py
+++ apps/scoobet/templatetags/scoobet_tags.py
@@ -1,13 +1,24 @@
+import re
+
 from django.core import urlresolvers
 from django import template
 from django.utils.translation import ugettext as _
 from django.db.models import Sum, Q
+from django.template.defaultfilters import linebreaksbr
 
 import scoobet
 from bet.models import Ticket, Bet
 from gsm.models import AbstractGsmEntity, Sport
 
 register = template.Library()
+
+URL_REGEXP = r'(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)((?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:".,?])))'
+
+@register.filter
+def render_comment(text):
+    text = re.sub(URL_REGEXP, r'<a href="\1">\2</a>', text)
+    text = linebreaksbr(text.strip())
+    return text
 
 @register.filter
 def group_activities(activities):
